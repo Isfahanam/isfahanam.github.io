41dcdd37683cc5ded523faee4a680909b68f6467
Max Base <MaxBaseCode@gmail.com>
2025-12-16 07:25:01 +0330

    use hash router in pro

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,6 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { Navigate } from "react-router-dom";
-
-import ProtectedRoute from "./routes/ProtectedRoute";
-import PublicRoute from "./routes/PublicRoute";
-import NotFoundPage from "./pages/NotFoundPage";
-import HomePage from "./pages/HomePage";
+import { BrowserRouter, HashRouter, Routes, Route } from "react-router-dom";
 import LoadingPage from "./pages/LoadingPage";
+import HomePage from "./pages/HomePage";
 import CitiesListPage from "./pages/CitiesListPage";
 import CityDetailPage from "./pages/CityDetailPage";
 import CityAttractionsPage from "./pages/CityAttractionsPage";
@@ -13,35 +8,28 @@ import AttractionDetailPage from "./pages/AttractionDetailPage";
 import FavoritesPage from "./pages/FavoritesPage";
 import SmartSuggestPage from "./pages/SmartSuggestPage";
 import MapPage from "./pages/MapPage";
+import NotFoundPage from "./pages/NotFoundPage";
 
 export default function App() {
   const basePath = import.meta.env.BASE_URL;
 
-  console.log("Base path is:", basePath);
+  const RouterComponent =
+    import.meta.env.PROD === true ? HashRouter : BrowserRouter;
 
   return (
-    <Router basename={basePath}>
+    <RouterComponent basename={basePath}>
       <Routes>
         <Route path="/" element={<LoadingPage />} />
-
         <Route path="/home" element={<HomePage />} />
-
         <Route path="/cities" element={<CitiesListPage />} />
-
         <Route path="/city/:cityId" element={<CityDetailPage />} />
-
         <Route path="/city/:cityId/attractions" element={<CityAttractionsPage />} />
-
         <Route path="/city/:cityId/attraction/:attractionId" element={<AttractionDetailPage />} />
-
         <Route path="/favorites" element={<FavoritesPage />} />
-
         <Route path="/smart-suggest" element={<SmartSuggestPage />} />
-
         <Route path="/map" element={<MapPage />} />
-
         <Route path="*" element={<NotFoundPage />} />
       </Routes>
-    </Router>
+    </RouterComponent>
   );
 }
